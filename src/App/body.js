31a7660701e5ce12ba6cd2a4a30f0ba637630a21
adriansv <adrian.sanchez@altia.es>
2020-03-03 11:21:37 +0100

    React.lazy y React.Suspense en la carga inicial del body

--- src/App/body.js
+++ src/App/body.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Route, Switch, withRouter} from 'react-router-dom';
-import Inicio from './Inicio';
-import Search from './Search';
-import Albums from './Albums';
-import Album from './Album';
-import Song from './Song';
-import InicioSesion from './InicioSesion';
-import User from './User';
+
+const Inicio = React.lazy(() => import('./Inicio'));
+const Search = React.lazy(() => import('./Search'));
+const Albums = React.lazy(() => import('./Albums'));
+const Album = React.lazy(() => import('./Album'));
+const Song = React.lazy(() => import('./Song'));
+const User = React.lazy(() => import('./User'));
+const InicioSesion = React.lazy(() => import('./InicioSesion'));
 
 export const Body = () => (
-    <div className="container">
-        <br/>
-        <Switch>
-            <Route exact path="/" component={Inicio}/>
-            <Route exact path="/search/:filtro" component={Search}/>
-            <Route exact path="/album/:id" component={Album}/>
-            <Route exact path="/song/:id/:album_id" component={Song}/>
-            <Route exact path="/song/:id" component={Song}/>
+    <div className="container">
+        <br/>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+                <Route exact path="/" component={Inicio}/>
+                <Route exact path="/search/:filtro" component={Search}/>
+                <Route exact path="/album/:id" component={Album}/>
+                <Route exact path="/song/:id/:album_id" component={Song}/>
+                <Route exact path="/song/:id" component={Song}/>
 
-            <Route path="/inicioSesion" component={InicioSesion}/>
-            <Route path="/user" component={User}/>
-            <Route path="/search" component={Search}/>
-            <Route path="/albums" component={Albums}/>
-            <Route path="/" component={Inicio}/>
-        </Switch>
+                <Route path="/inicioSesion" component={InicioSesion}/>
+                <Route path="/user" component={User}/>
+                <Route path="/search" component={Search}/>
+                <Route path="/albums" component={Albums}/>
+                <Route path="/" component={Inicio}/>
+            </Switch>
+        </Suspense>
     </div>
 );
 
