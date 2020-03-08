93f55661012593bed9aee6d89b7553b9f1918fe3
adriansv87 <hadrycarretera@gmail.com>
2020-03-08 14:27:43 +0100

    Configuraciones

--- src/sw.js
+++ src/sw.js
@@ -14,7 +14,7 @@ self.addEventListener('install', (event) => {
   
   // Evento activate
   self.addEventListener('activate', (e) => {
-    console.log('activado');
+    console.log('El nuevo service worker se ha activado');
   });
   
   // Evento de mensajes
@@ -25,3 +25,32 @@ self.addEventListener('install', (event) => {
       self.skipWaiting();
     }
   });
+
+// ----------------------------------------------------------------------------
+// self.addEventListener('install', (event) => {
+//   console.log("Service worker!")
+
+//   event.waitUntil(
+//     caches.open(cacheName)
+//       .then((cache) => {
+//         console.log("cache esta abierta");
+//         return cache.addAll(filesToCache);
+//       })
+//   )
+// })
+
+// self.addEventListener('activate', (event) => {
+//   console.log("el nuevo service worker se ha activado")
+// });
+
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then((response) => {
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       })
+//   )
+// });
